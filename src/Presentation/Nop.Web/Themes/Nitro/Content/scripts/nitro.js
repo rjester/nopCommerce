adc22884f4349395203bd3c477c9efa6142050f1
Russ Jester <rjester@gmail.com>
2017-07-30 12:26:00 -0400

    add nitro theme

---
+++ src/Presentation/Nop.Web/Themes/Nitro/Content/scripts/nitro.js
@@ -0,0 +1,39 @@
+﻿(function () {
+    $(document).ready(function () {
+        var responsiveAppSettings = {
+            isEnabled: true,
+            themeBreakpoint: 980,
+            isSearchBoxDetachable: true,
+            isHeaderLinksWrapperDetachable: true,
+            doesDesktopHeaderMenuStick: true,
+            doesScrollAfterFiltration: true,
+            doesSublistHasIndent: true,
+            displayGoToTop: true,
+            hasStickyNav: true,
+            selectors: {
+                menuTitle: ".menu-title",
+                headerMenu: ".header-menu",
+                closeMenu: ".close-menu",
+                movedElements: ".admin-header-links, .header, .responsive-nav-wrapper, .master-wrapper-content, .footer, .slider-wrapper",
+                sublist: ".header-menu .sublist",
+                overlayOffCanvas: ".overlayOffCanvas",
+                withSubcategories: ".with-subcategories",
+                filtersContainer: ".nopAjaxFilters7Spikes",
+                filtersOpener: ".filters-button span",
+                searchBoxOpener: ".search-wrap > span",
+                searchBox: ".search-box.store-search-box",
+                searchBoxBefore: ".page-title span, .page-title h1",
+                navWrapper: ".responsive-nav-wrapper",
+                navWrapperParent: ".responsive-nav-wrapper-parent",
+                headerLinksOpener: "#header-links-opener",
+                headerLinksWrapper: ".header-links-wrapper",
+                headerLinksWrapperMobileInsertAfter: ".header",
+                headerLinksWrapperDesktopPrependTo: ".header-centering",
+                shoppingCartLink: ".shopping-cart-link",
+                overlayEffectDelay: 300
+            }
+        };
+
+        sevenSpikes.initResponsiveTheme(responsiveAppSettings);
+    });
+})();
